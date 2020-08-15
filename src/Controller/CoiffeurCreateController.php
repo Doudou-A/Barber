c787c438c60a5b9c781b94de9149d86e1c34d664
Adel <adeldoudou1996@gmail.com>
2020-08-15 20:54:59 +0200

    $e

--- src/Controller/CoiffeurCreateController.php
+++ src/Controller/CoiffeurCreateController.php
@@ -53,7 +53,7 @@ class CoiffeurCreateController extends AbstractController
                             $newFileName
                         );
                     } catch (FileException $e) {
-                        echo("move don't work");
+                        echo($e);
                         die;
                         // ... handle exception if something happens during file upload
                     }
