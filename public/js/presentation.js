eb588b195842209145e5611b017d8c520749c88c
Adel <adeldoudou1996@gmail.com>
2020-08-20 18:02:43 +0200

    presentation

--- public/js/presentation.js
+++ public/js/presentation.js
@@ -8,8 +8,8 @@ $(function () {
 });
 $(function () {
     $('body').on('click', '.zoomPresentationSnd', function () {
-        $(this).removeClass('col-12').removeClass('position-absolute').removeClass('zoomPresentationSnd').removeClass('h-75').addClass('col-lg-3').addClass('col-lg-5').addClass('hoverWB').addClass('hiddenPresentation').addClass('zoomPresentation');
+        $(this).removeClass('col-12').removeClass('position-absolute').removeClass('zoomPresentationSnd').removeClass('h-75').addClass('col-lg-3').addClass('col-5').addClass('hoverWB').addClass('hiddenPresentation').addClass('zoomPresentation');
         $('.hiddenPresentation').removeClass('d-none').addClass('d-flex');
-        
+
     })
 });
