67b2224a9bc7588e6cc9de086c1c1b17373cf471
Adel <adeldoudou1996@gmail.com>
2020-08-20 17:56:57 +0200

    ok

--- public/js/presentation.js
+++ public/js/presentation.js
@@ -10,5 +10,6 @@ $(function () {
     $('body').on('click', '.zoomPresentationSnd', function () {
         $(this).removeClass('col-12').removeClass('position-absolute').removeClass('zoomPresentationSnd').removeClass('h-75').addClass('col-lg-3').addClass('col-lg-5').addClass('hoverWB').addClass('hiddenPresentation').addClass('zoomPresentation');
         $('.hiddenPresentation').removeClass('d-none').addClass('d-flex');
+        
     })
 });
