b59b0030d74778fb9993e7c3dc61cf8d03435116
Adel <adeldoudou1996@gmail.com>
2020-08-20 17:52:52 +0200

    presentation

--- public/js/presentation.js
+++ public/js/presentation.js
@@ -2,13 +2,13 @@
 //zoomPresentation
 $(function () {
     $('body').on('click', '.zoomPresentation', function () {
-        $(this).removeClass('col-lg-3').removeClass('hoverWB').removeClass('hiddenPresentation').removeClass('zoomPresentation').addClass('col-10').addClass('offset-1').addClass('position-absolute').addClass('zoomPresentationSnd').addClass('h-50');
+        $(this).removeClass('col-lg-3').removeClass('hoverWB').removeClass('hiddenPresentation').removeClass('zoomPresentation').addClass('col-12').addClass('position-absolute').addClass('zoomPresentationSnd').addClass('h-50');
         $('.hiddenPresentation').addClass('d-none').removeClass('d-flex');
     })
 });
 $(function () {
     $('body').on('click', '.zoomPresentationSnd', function () {
-        $(this).removeClass('col-10').removeClass('offset-1').removeClass('position-absolute').removeClass('zoomPresentationSnd').removeClass('h-50').addClass('col-lg-3').addClass('hoverWB').addClass('hiddenPresentation').addClass('zoomPresentation');
+        $(this).removeClass('col-12').removeClass('position-absolute').removeClass('zoomPresentationSnd').removeClass('h-75').addClass('col-lg-3').addClass('hoverWB').addClass('hiddenPresentation').addClass('zoomPresentation');
         $('.hiddenPresentation').removeClass('d-none').addClass('d-flex');
     })
 });
