c764f78fed94bbaeb9694ecdec7286e0e2f7e242
Adel <adeldoudou1996@gmail.com>
2020-12-16 22:17:38 +0100

    fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    /**
+     *
+     * @var UserPasswordEncoderInterface
+     *
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        $user = new User();
+        $user->setUsername('admin')
+            ->setEmail('admin@gmail.com')
+            ->setPassword($this->encoder->encodePassword($user, 'password'))
+            ->setRoles(['ROLE_ADMIN'])
+            ->setName('admin')
+            ->setFirstName('admin')
+            ->setDateCreated(date())
+            ->setNumber(1);
+
+        $manager->persist($user);
+
+        $user->setUsername('user')
+            ->setEmail('user@gmail.com')
+            ->setPassword($this->encoder->encodePassword($user, 'password'))
+            ->setRoles(['ROLE_USER'])
+            ->setName('user')
+            ->setFirstName('user')
+            ->setDateCreated(date())
+            ->setNumber(2);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
